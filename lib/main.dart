bf60d27c585e4e5ca2bc1f15b08d206c156b2a46
Roberto <pcarltomy@gmail.com>
2026-01-07 14:28:55 -0500

    Correction

--- lib/main.dart
+++ lib/main.dart
@@ -20,116 +20,121 @@ class PageAccueil extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.bleu,
-        title: Text('Magazine Infos'),
+        backgroundColor: Colors.pink,
+        title: Text('Magazine Infos', style: TextStyle(color: Colors.white)),
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
-        actions: IconButton(icon: Icon(Icons.search), onPressed: () {}),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+            color: Colors.white,
+          ),
+        ],
       ),
       body: Column(
-        children[
-         Image.asset('assets/images/magazineInfo.jpg')
-         PartieTitre(),
-         PartieTexte(),
-         PartieIcone(),
-         PartieRubrique(),
+        children: [
+          Image.asset('assets/images/magazineInfo.jpg'),
+          PartieTitre(),
+          PartieTexte(),
+          PartieIcone(),
+          PartieRubrique(),
         ],
       ),
     );
   }
 }
+
 class PartieTitre extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      Widget: double.infinity,
+      width: double.infinity,
       padding: EdgeInsets.all(20),
       child: Column(
-        crossAxisAlignement: CrossAxisAlignement.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            'Bienvenue au Magazin Infos',
-            style: Textstyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
+            'Bienvenue au Magazine Infos',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Text(
-            "Votre Magazine numérique, votre source d'inspiration",
-           style: Textstyle(
-            fontSize: 16,
-            color: Colors.grey,
+            "Votre magazine numérique, votre source d'inspiration",
+            style: TextStyle(fontSize: 16, color: Colors.grey),
           ),
         ],
       ),
-    ),
+    );
   }
 }
 
-class PartieTexte extends StatelessWidget{
+class PartieTexte extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
       child: Text(
-        'Magazine Infos est un magazine numérique moderne qui propose des articles variés sur la culture, la technologie, la mode et l’actualité. Il s’adresse à tous les passionnés de lecture.',
-        style: Textstyle(
-          fontSize: 15,
-        ),
+        'Magazine Infos est un magazine numérique moderne qui propose des articles variés sur la culture, la technologie, la mode et l’actualité.',
+        style: TextStyle(fontSize: 15),
       ),
-    ),
+    );
   }
 }
-class PartieIcone extends StatelessWidget{
+
+class PartieIcone extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-     padding: EdgeInsets.only(bottom:10),
-     child : Row(
-       mainAxiAlignement: MainAxiAlignement.spaceEvenly,
-       children: [
-         _itemIcon(Icons.phone, 'TEL'),
-         _itemIcon(Icons.email, 'EMAIL'),
-         _itemIcon(Icons.share, 'PATAGE'),
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _itemIcon(Icons.phone, 'TEL'),
+          _itemIcon(Icons.email, 'EMAIL'),
+          _itemIcon(Icons.share, 'PARTAGE'),
         ],
       ),
-    ),
+    );
   }
+
   Widget _itemIcon(IconData icon, String text) {
-  return Column(
-    children: [
-      Icon(icon, color :Colors.pink),
-      SizedBox(height: 5),
-      Text(
-        text,
-        style: TextStyle(color: Colors.pink),
-        ),
+    return Column(
+      children: [
+        Icon(icon, color: Colors.pink),
+        SizedBox(height: 5),
+        Text(text, style: TextStyle(color: Colors.pink)),
       ],
-    ),
+    );
   }
-class PartieRubrique extends StatelessWidget{
+}
+
+class PartieRubrique extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
-      padding : EdgeInsets.symetric(horizontal: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
       child: Row(
-       children[
-         Expanded(
-           child: ClipRRect(
-            borderRadius : BorderRaduis.circular(10),
-            child: Image.asset('assert/images/press.jpg'),
-           ),
-          ),
-          SizedBox(width :10),
+        children: [
           Expanded(
             child: ClipRRect(
-              borderRadius: BorderRaduis.circular(10),
-              child : Image.asset('assets/images/mode.jpg'),
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset('assets/images/press.jpg'),
+            ),
+          ),
+          SizedBox(width: 10),
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset('assets/images/mode.jpg'),
             ),
           ),
         ],
       ),
-    ),
+    );
   }
 }
